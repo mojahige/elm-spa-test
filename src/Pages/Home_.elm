52c6fc0e5f9d031347ad34769e10ba0115e4016c
mojahige <mojahige@gmail.com>
2022-09-28 23:38:55 +0900

    Tailwind CSS

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -2,6 +2,7 @@ module Pages.Home_ exposing (page, view)
 
 import Helpers.Url exposing (generateUrlString)
 import Html
+import Html.Attributes exposing (class)
 import Page exposing (Page)
 import Request exposing (Request)
 import Shared
@@ -20,8 +21,8 @@ view : Request -> View msg
 view req =
     { title = "Homepage"
     , body =
-        [ Html.div []
-            [ Html.h1 [] [ Html.text "elm-spa test" ]
+        [ Html.main_ [ class "p-8" ]
+            [ Html.h1 [ class "italic font-bold text-3xl" ] [ Html.text "elm-spa test" ]
             , Html.p [] [ Html.text "Hello, world!" ]
             , Html.p [] [ Html.text (generateUrlString req.url) ]
             ]
